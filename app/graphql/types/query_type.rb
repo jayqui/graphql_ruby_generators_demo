dad066a92fa1a25acad45700c2837f315ef0ade1
Jay Quigley <quigley.james.g@gmail.com>
2020-11-10 18:59:15 -0800

    Add (non-functional) basic :posts field on QueryType

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,9 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :posts, [Types::PostType], 'Returns all blog posts', null: false
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def posts
+      Post.all
     end
   end
 end
